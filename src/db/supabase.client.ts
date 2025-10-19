be53db8a74f06eba2f7663d5a6aaa12e0866a076
bartosz-kepka-htd <bartosz.kepka@htdevelopers.com>
2025-10-19 17:53:26 +0200

    chore: add dev user id, export supabase client type

--- src/db/supabase.client.ts
+++ src/db/supabase.client.ts
@@ -6,4 +6,7 @@ const supabaseUrl = import.meta.env.SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.SUPABASE_KEY;
 
 export const supabaseClient = createClient<Database>(supabaseUrl, supabaseAnonKey);
+export type SupabaseClient = typeof supabaseClient;
+
+export const DEV_USER_ID = '34c1f4e0-ddab-4bc9-ab22-eb368cefbaaa';
 
