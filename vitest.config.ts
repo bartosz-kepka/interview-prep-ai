bcfb42a29c88b3171282a9fc5af635268fd1c697
bartosz-kepka-htd <bartosz.kepka@htdevelopers.com>
2025-10-27 22:51:40 +0100

    feat: implement first unit test

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,7 @@ import { getViteConfig } from 'astro/config';
 
 export default getViteConfig({
   test: {
-    include: ['tests/unit/**/*.spec.ts'],
+    include: ['tests/unit/**/*.spec.{ts,tsx}'],
     environment: 'jsdom',
     setupFiles: ['./vitest.setup.ts'],
     globals: true,
